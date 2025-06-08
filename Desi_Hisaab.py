3dbca0b6c705c4ef82e2e8105f40b819d6d12e74
Reetesh Kumar Shukla <reeteshsnshukla@gmail.com>
2025-06-08 16:13:00 +0530

    Update Desi_Hisaab.py

--- Desi_Hisaab.py
+++ Desi_Hisaab.py
@@ -14,25 +14,24 @@ if "add_more" not in st.session_state:
     st.session_state.add_more = True
 
 st.title("Desi Hisaab")
-person = st.number_input("How many person are there in this bill?",step=1, format='%d', placeholder= 'like, 1,2,3,4,....')
-size  = st.number_input("How many places or  you done (in number)",step=1, format='%d', placeholder= 'like, 1,2,3,4,....')
+
+person = st.number_input("How many persons?",step=1, format='%d', placeholder= 'like, 1,2,3,4,....')
+size  = st.number_input("How many places you spend?",step=1, format='%d', placeholder= 'like, 1,2,3,4,....')
 for i in range(int(size)):
-    key = st.text_input(f"Type the name of the product you spend money on (in strings) as Key".capitalize(), placeholder= 'Samsoa, Maggie, Coke, ...', key= f'item_{i}')
-    value = st.number_input(f"Enter the total amount you spend on that product (integer) as Value (in Rs.)", placeholder=' Rs. 12,40,50', key= f'amount_{i}')
+    key = st.text_input("Name of the product", placeholder= 'Samsoa, Maggie, Coke, ...', key= f'item_{i}')
+    value = st.number_input("Enter Price of the product Rs.",step=1, format= "%d", placeholder=' Rs. 12,40,50', key= f'amount_{i}')
     if key:
         st.session_state.dict[key] = value
 st.write("---")
 
 if st.session_state.add_more:
     with st.form(key = "add_more_item"):
-        key = st.text_input(f"Type the name of the product you spend money on (in strings) as Key".capitalize(), placeholder='Samsoa, Maggie, Coke, ...', key=f'new_item_{st.session_state.extra_count}')
-        value = st.number_input(f"Enter the total amount you spend on that product (integer) as Value (in Rs.)", placeholder=' Rs. 12,40,50', key=f'new_amount_{st.session_state.extra_count}')
+        key = st.text_input("Name of the product", placeholder= 'Samsoa, Maggie, Coke, ...', key=f'new_item_{st.session_state.extra_count}')
+        value = st.number_input("Enter Price of the product Rs.",step=1, format= "%d", placeholder=' Rs. 12,40,50', key= f'new_amount_{st.session_state.extra_count}')
         submitted = st.form_submit_button("Add Item")
         if submitted and key:
             st.session_state.dict[key] = value
-            # for key, value in st.session_state.dict.items():
-            #     st.session_state.dict[key] = value
-            st.session_state.extra_count += 1
+            # st.session_state.extra_count += 1
     st.markdown("### Your Items")
     for key, value in st.session_state.dict.items():
         st.write(f'**{key.capitalize()}** -> _Rs. {value}_')
@@ -40,14 +39,12 @@ st.write("---")
                 # st.write(f' {key} : : Rs. {value}')
                 # st.session_state.sum = st.session_state.sum + value
 if st.button('Finish and Split Bill'):
+    st.session_state.add_more= False
     st.session_state.sum = sum(st.session_state.dict.values())
     if st.session_state.sum != 0 and person >0:
-        st.session_state.add_more = False
-        st.write("Okay! I'm generating your bill now.")
-        st.write("Your bill is generating please wait....")
-        with st.spinner("Please wait...."):
-            time.sleep(3)
         div = st.session_state.sum / person
-        st.write("")
-        st.write(f'Total Amount: Rs. {st.session_state.sum}')
-        st.write(f'Each person have to pay: Rs.{div: .2f}/-per head')
+        st.write("Okay Your bill is now generating")
+        with st.spinner("Please Wait"):
+            time.sleep(3)
+        st.write(f"Your Total Amount: Rs. {st.session_state.sum}")
+        st.write(f'The Splitted bill is here: Rs. {div}/- Per Head')
